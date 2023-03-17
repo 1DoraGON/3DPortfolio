af63039eda2337265f955082b753ea194d0c1126
Doragon <moulaykadii2001@gmail.com>
2023-03-17 23:14:11 +0000

    fixing some media query issues

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
@@ -22,14 +22,32 @@ const Computers = () => {
       />
       <primitive
       object={computer.scene} 
-      scale={0.6}
-      position={[0,-3,-1.5]}
+      scale={isMobile ? 0.5 : 0.7}
+      position={isMobile ? [0,-2.5,-1]:[0,-3.5,-1]}
       rotation={[-0.01,-0.2,-0.1]}/>
     </mesh>
   )
 }
 
 const ComputersCanvas =() => {
+  const [isMobile,setIsMobile] = useState(false)
+
+  useEffect(()=>{
+    const mediaQuery = window.matchMedia('(max-width: 700px)');
+    setIsMobile(mediaQuery.matches)
+    
+    const handleMQC = (e) => {
+      setIsMobile(e.matches);
+      
+    }
+    mediaQuery.addEventListener('change',
+    handleMQC)
+    console.log(isMobile);
+    return () => {
+      mediaQuery.removeEventListener('change',
+      handleMQC);
+    }
+  },[])
   return (
     <Canvas
     frameloop='demand'
@@ -41,7 +59,7 @@ const ComputersCanvas =() => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
